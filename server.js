3fba5d6cff9fcd4c0a8ad4d9c257d17b7816806f
michaeltranquocbao <micaetranquocbao2001@gmail.com>
2023-08-05 17:09:34 +0700

    init app

---
+++ server.js
@@ -0,0 +1,12 @@
+// import app from "../src/app";
+const app = require("./src/app");
+
+const PORT = 2023;
+
+const server = app.listen(PORT, () => {
+  console.log(`WSV eCommerce start with ${PORT}`);
+});
+
+process.on("SIGINT", () => {
+  server.close(() => console.log(`Exit Server Express`));
+});
---
+++ src/app.js
@@ -0,0 +1,17 @@
+// import express from "express";
+const express = require("express");
+
+const app = express();
+
+// const app
+
+// init middlewares
+
+// init db
+
+// init routes
+
+// handling error
+
+module.exports = app;
+// export default app;
